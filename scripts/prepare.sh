af3f754e6d88b248cb830df6a1957b36c91b48b5
Kolja Ptaberg <ptashym@gmail.com>
2020-03-04 14:38:37 +0300

    fix path

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -32,8 +32,8 @@ clone_project()
 stage "Clone projects"
 
 clone_project https://github.com/ptaberg/sc-machine.git sc-machine scp_stable
-clone_project https://github.com/Ivan-Zhukau/sc-web.git sc-web master
-clone_project https://github.com/ptaberg/ims.ostis.kb.git ims.ostis.kb master
+clone_project https://github.com/ptaberg/sc-web.git sc-web master
+clone_project https://github.com/ShunkevichDV/ims.ostis.kb.git ims.ostis.kb master
 
 stage "Prepare projects"
 
